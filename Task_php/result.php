688613e63fe8b122bea55c48a3d0521dccef8e45
YuvikaCatalyst <156080459+YuvikaCatalyst@users.noreply.github.com>
2024-02-05 11:35:37 +0530

    Add files via upload

---
+++ Task_php/result.php
@@ -0,0 +1,22 @@
+<?php
+
+ include('database.php');
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $first_name = $_POST['first_name_val'];
+        $middle_name = $_POST['middle_name_val'];
+        $last_name = $_POST['last_name_val'];
+
+       $file_name = '';
+        if (isset($_FILES['upload_file'])) {
+            $file_name = $_FILES['upload_file']['name'];
+            $file_tmp = $_FILES['upload_file']['tmp_name'];
+            move_uploaded_file($file_tmp, "assets/uploads/" . $file_name);
+        }
+
+        $sql = "INSERT INTO table1 (first_name, middle_name, last_name, file_name) VALUES ('$first_name', '$middle_name',' $last_name', '$file_name')";
+        mysqli_query($conn, $sql);
+       
+    }
+?>
+
